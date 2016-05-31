10ab2f12d8cb2203bef086b2b014ce7102d6b267
Susan Potter <me@susanpotter.net>
2016-05-30 23:23:47 -0500

    Add 'Eliminate Bottom' reasoning post; add recursion draft

--- shell.nix
+++ shell.nix
@@ -2,8 +2,16 @@
 , ... }:
 let
 
-  inherit (pkgs) stdenv scala sbt nodejs jekyll python27Packages;
+  # tools
+  inherit (pkgs) stdenv scala sbt nodejs ruby bundler python27Packages;
 
+  # libraries/build dependencies
+  inherit (pkgs) lib lzip libxml2 libxslt file libffi;
+
+  nokogiriCfg = lib.concatStringsSep " " [
+    "--with-xml2-config=${libxml2}/bin/xml2-config"
+    "--with-xslt-config=${libxslt}/bin/xslt-config"
+  ];
 in
 stdenv.mkDerivation {
   name = "funops";
@@ -11,11 +19,20 @@ stdenv.mkDerivation {
     scala
     sbt
     nodejs
-    jekyll
+    ruby
+    bundler
+    libffi
+    lzip
+    libxml2
+    libxslt
     python27Packages.pygments
   ];
 
   shellHook = ''
-    ${sbt}/bin/sbt tut
+    export LD_LIBRARY_PATH="${lzip}/lib"
+    export C_INCLUDE_PATH="${lzip}/include"
+    bundle config --local path .bundle
+    bundle config --local build.nokogiri -- ${nokogiriCfg}
+    bundle install
   '';
 }
